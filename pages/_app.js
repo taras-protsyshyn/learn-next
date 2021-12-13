426db5698a71bd790b268514c3a36b6cf99285b6
Taras <protsyshyn.taras@coaxsoft.com>
2021-12-13 07:59:35 +0200

    add loader indicator

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import NextNProgress from "nextjs-progressbar";
 
 import { MainLayout } from "../layouts/MainLayout";
 
@@ -10,6 +11,13 @@ export default function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <MainLayout>
+        <NextNProgress
+          color="#29D"
+          startPosition={0.3}
+          stopDelayMs={200}
+          height={3}
+          showOnShallow={true}
+        />
         <Component {...pageProps} />
       </MainLayout>
     </>
